9163a3d111268b3bcdb99cb775218d4ef4e184b8
peninah98 <peninangizwena@gmail.com>
2023-07-24 10:16:32 +0200

    Updates

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,34 @@
-console.log("Exercise 3 - Timer");
+const myTimer = document.querySelector('#myTimer')
+const inputValue = document.querySelector('.inputValue')
+const decrease = document.querySelector('.decrease')
+const increase = document.querySelector('.increase')
+const resetBtn = document.querySelector('.resetBtn')
+const stopBtn = document.querySelector('.stopBtn')
+const message = document.querySelector('.message')
+
+
+let initial = inputValue.value
+message.innerHTML = initial
+
+
+const countdown = setInterval(()=>{
+    initial--;
+    message.innerHTML = initial;
+    if(initial <= 0 || initial <1){
+        clearInterval(countdown);
+        hideButtons()
+    }
+},1000)
+
+
+increase.addEventListener('click',function(){
+    initial += 1;
+    time.innerHTML = initial;
+})
+decrease.addEventListener('click',function(){
+    initial -= 1;
+    time.innerHTML = initial;
+})
+
+
+
