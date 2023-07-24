de7a9445d1c49accf1617e393026b7ce34ecf5fd
peninah98 <peninangizwena@gmail.com>
2023-07-24 10:58:14 +0200

    Exercise 3 updates

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,34 +1,53 @@
 const myTimer = document.querySelector('#myTimer')
-const inputValue = document.querySelector('.inputValue')
+const inputValue = document.querySelector('.inputValue').value
 const decrease = document.querySelector('.decrease')
 const increase = document.querySelector('.increase')
 const resetBtn = document.querySelector('.resetBtn')
-const stopBtn = document.querySelector('.stopBtn')
+const startBtn  = document.querySelector('#startBtn')
 const message = document.querySelector('.message')
 
 
-let initial = inputValue.value
+let initial = 30
 message.innerHTML = initial
 
 
 const countdown = setInterval(()=>{
     initial--;
-    message.innerHTML = initial;
+    message.innerHTML = initial
     if(initial <= 0 || initial <1){
         clearInterval(countdown);
-        hideButtons()
+        resetBtn.innerHTML='stop'
+        startBtn.innerHTML='RESET'
+        
     }
 },1000)
 
 
+//
+
 increase.addEventListener('click',function(){
     initial += 1;
-    time.innerHTML = initial;
+    message.innerHTML = initial;
 })
 decrease.addEventListener('click',function(){
     initial -= 1;
-    time.innerHTML = initial;
+    message.innerHTML = initial;
+    
+
 })
+
+
+startBtn.addEventListener('click',function(){
+    message.innerHTML = initial;
+})
+
+resetBtn.addEventListener('click',function(){
+    initial === 0;
+    message.innerHTML = initial;
+})
+
+
+
 
 
 
