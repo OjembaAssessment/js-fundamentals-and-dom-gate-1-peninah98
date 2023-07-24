b19c31e8511e4b6e2d290abe8227970842ecf11b
peninah98 <peninangizwena@gmail.com>
2023-07-24 11:38:54 +0200

    Exercise 1

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,2 +1,16 @@
-console.log("Exercise 1 - Word Highlighter");
-console.log('starting')
+const text = document.getElementById('myParagraph')
+const allText= text.firstChild.nodeValue
+
+let splitted = allText.split(" ")
+let splittedArray = Array.from(splitted)
+const uniqueWords = new Set(splittedArray);
+const sorted = splittedArray.sort()
+console.log(sorted)
+
+uniqueWords.forEach((word)=>{
+    const startingPosition = sorted.indexOf(word)
+    const lastPosition = sorted.lastIndexOf(word);
+    const count = lastPosition - startingPosition + 1;
+    console.log(word, ':', count);
+})
+
