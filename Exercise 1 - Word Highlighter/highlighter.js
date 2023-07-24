78489ef9e05a9908b9c8537d2d67f1f5341a8606
peninah98 <peninangizwena@gmail.com>
2023-07-24 13:01:44 +0200

    update

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,6 +1,9 @@
 const text = document.getElementById('myParagraph')
 const allText= text.firstChild.nodeValue
 
+let mostOccuringWord = 0
+let result = []
+
 let splitted = allText.split(" ")
 let splittedArray = Array.from(splitted)
 const uniqueWords = new Set(splittedArray);
@@ -12,5 +15,38 @@ uniqueWords.forEach((word)=>{
     const lastPosition = sorted.lastIndexOf(word);
     const count = lastPosition - startingPosition + 1;
     console.log(word, ':', count);
+    
+
+
+
+if(count > mostOccuringWord){
+   mostOccuringWord = count
+    result.push(word)
+   
+    
+}
+
+if(count === mostOccuringWord){
+    console.log('hahahahahahah    ',word)
+   
+  
+    result.push(word)
+}
+
+
+
+
 })
+
+
+result.forEach((word) => {
+    for (let i = 0; i < splittedArray.length; i++) {
+      if (splittedArray[i] === word) {
+        splittedArray[i] = `<span style="background-color: yellow;">${word}</span>`;
+      }
+    //   if(splittedArray[i] === word &&splittedArray[i].charAt(0) == )
+    }
+  });
+  
+  text.innerHTML = splittedArray.join(' ');
 
